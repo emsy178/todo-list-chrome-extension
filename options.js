6f938b123dde6d31169035b2e48545364c0421d8
emsy178 <emsy178>
2024-04-28 20:40:56 +0000

    final version

--- options.js
+++ options.js
@@ -2,59 +2,81 @@
 const itemContainerElement = document.getElementById("item-container");
 const btnElement = document.getElementById("btn-add");
 const toDoInputElement = document.getElementById("to-do");
+const validateBtnElement = document.getElementById("validate")
+const deleteBtnElement =  document.getElementById("delete")
 
-// Ajout d' un ecouteur d' evenement pour le bouton "add"
+// Ajout d' un écouteur d' événement pour le bouton "add"
 btnElement.addEventListener("click", () => {
+  // Verifier si l' input est vide
   if (!toDoInputElement.value) {
     return;
   }
-  // recuperer le valeur de l' input 
+  // recuperer la valeur de l' input
   const value = toDoInputElement.value;
 
   chrome.storage.local.get(["task"], (result) => {
+    // Verifier si une tâche  a été  créée
     if (!result.task) {
       console.log("La clé 'task' n'existe pas encore dans le stockage local.");
+      // Creation  d' un array conntenant un objet qui a son tour contient le nom de la tache et son status
       const arrayTask = [{ task: value, done: false }];
+
+      // creation d' un objet task
       const task = {
         tasks: arrayTask,
       };
       console.log(task);
+
+      // Enregistrer la tâche dans le stockage local de l'extension Chrome
       chrome.storage.local.set({ task: task }, () => {
         console.log("la tache vient d' etre cree :) ");
       });
+
+      // Récupérer l' index du premier element du tableau
+      const index = arrayTask.length - 1;
+      // Initialiser le champ de l' input to do a vide
       toDoInputElement.value = "";
-      itemContainerElement.innerHTML += ` <div class="item" data-index = "${index}" >
+      // Mettre à jour l' élément conteneur des tâches
+      itemContainerElement.innerHTML = ` <div class="item" data-index = "${index}" >
       <span>${value}</span>
       <div class="btn-container">
-          <span class="material-symbols-outlined">
+          <span class="material-symbols-outlined"  id="validate">
               done
           </span>
-          <span class="material-symbols-outlined">
+          <span class="material-symbols-outlined" id="delete">
               close
           </span>
       </div>
     </div>`;
     } else {
-      console.log("voici le console.log de result");
       console.log(result.task);
+      // Récupérer le tableau contenant les tâches depuis  le chrome.storage
       let task = result.task.tasks;
       console.log(task);
+
+      // Creation d' une nouvelle  tâches
       task.push({ task: value, done: false });
+
+      // Récupérer   l' index du dernier element du tableau
       const index = task.length - 1;
+      // Creation d' un objet task
       task = {
         tasks: task,
       };
+      // Enregistrer la tâche dans le stockage local de l'extension Chrome
       chrome.storage.local.set({ task: task }, () => {
         console.log("la tache vient d' etre mise a jour  :)");
       });
+      // Initialiser le champ de l' input to do a vide
       toDoInputElement.value = "";
+      // Mettre à jour l' élément conteneur des tâches
       itemContainerElement.innerHTML += ` <div class="item" data-index = "${index}" >
       <span>${value}</span>
       <div class="btn-container">
-          <span class="material-symbols-outlined">
+          <span class="material-symbols-outlined" id="validate">
               done
           </span>
-          <span class="material-symbols-outlined">
+          <span class="material-symbols-outlined" id="delete">
               close
           </span>
       </div>
@@ -83,10 +105,10 @@ function displayToDo() {
       itemContainerElement.innerHTML += ` <div class="item" data-index = "${task}" >
       <span>${tasks[task].task}</span>
       <div class="btn-container">
-          <span class="material-symbols-outlined">
+          <span class="material-symbols-outlined" id="validate">
               done
           </span>
-          <span class="material-symbols-outlined">
+          <span class="material-symbols-outlined" id="delete">
               close
           </span>
       </div>
@@ -94,3 +116,7 @@ function displayToDo() {
     }
   });
 }
+
+
+console.log(validateBtnElement,"button validate")
+console.log(deleteBtnElement)
